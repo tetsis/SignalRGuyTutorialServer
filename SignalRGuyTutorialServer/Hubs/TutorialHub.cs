6f7ece5314e4e8061b0b86e8d48e350e6812b981
tetsis <hitoshi.zin.nakao@gmail.com>
2022-02-19 03:18:42 +0900

    プロジェクト ファイルを追加します。

---
+++ SignalRGuyTutorialServer/Hubs/TutorialHub.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using SignalRGuyTutorialServer.Models;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SignalRGuyTutorialServer.Hubs
+{
+    public class TutorialHub : Hub
+    {
+        public Task Ping()
+        {
+            return Clients.Caller.SendAsync("Pong");
+        }
+
+        public Task SendMessage(string message)
+        {
+            return Clients.Caller.SendAsync("ReceiveMessage", message);
+        }
+
+        public async Task JoinToGroup(string group)
+        {
+            await Groups.AddToGroupAsync(Context.ConnectionId, group);
+        }
+
+        public async Task SendMessageToGroup(string group, string user, string message)
+        {
+            await Clients.Group(group).SendAsync("ReceiveMessageFromGroup", group, user, message);
+        }
+
+        public async Task LeaveFromGroup(string group)
+        {
+            await Groups.RemoveFromGroupAsync(Context.ConnectionId, group);
+        }
+
+        public Task GetWeek(Date date)
+        {
+            var datetime = new DateTime(date.Year, date.Month, date.Day);
+            var dayOfWeek = datetime.ToString("dddd", new CultureInfo(date.Locale));
+            return Clients.Caller.SendAsync("GetWeek", dayOfWeek);
+        }
+
+        public Task CalcTotal(int[] numbers)
+        {
+            var total = numbers.Sum();
+            return Clients.Caller.SendAsync("CalcTotal", total);
+        }
+    }
+}
---
+++ SignalRGuyTutorialServer/Models/Date.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SignalRGuyTutorialServer.Models
+{
+    public class Date
+    {
+        public int Year { get; init; }
+        public int Month { get; init; }
+        public int Day { get; init; }
+        public string Locale { get; init; }
+    }
+}
